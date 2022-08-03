7ca26351cc3653f79f06b833851ee8075a4eb785
Creeland <cree@provinsal.com>
2022-08-03 14:32:40 -0500

    feat: extend mutation to include slug and production process

--- scripts/sanity-create-course.js
+++ scripts/sanity-create-course.js
@@ -1,60 +1,70 @@
-let { nanoid } = await npm("nanoid");
+let { nanoid } = await npm("nanoid")
 
-let { selectedIllustrator, eggheadSanityClient } = await lib("sanity");
-let { queryEggheadCourse, eggheadGraphQLClient } = await lib("egghead");
+let { selectedIllustrator, eggheadSanityClient } = await lib("sanity")
+let { queryEggheadCourse, eggheadGraphQLClient } = await lib("egghead")
 
-const courseID = await arg("Enter Course slug: ");
+const courseID = await arg("Enter Course slug: ")
 
-let illustrator = await selectedIllustrator();
+let illustrator = await selectedIllustrator()
 
 let sanityIllustrator = (tmpIllustrator) => {
-  if (!tmpIllustrator) return;
+  if (!tmpIllustrator) return
   return {
     _key: nanoid(),
     _ref: `collaborator-illustrator-${tmpIllustrator}`,
     _type: "reference",
-  };
-};
+  }
+}
 
-let railsData = await queryEggheadCourse(courseID);
+let railsData = await queryEggheadCourse(courseID)
 
 function graphQlToSanity(railsData) {
-  let { id, title, description, image, path, resources, instructor } =
-    railsData.course;
+  let { id, title, slug, description, image, path, resources, instructor } =
+    railsData.course
 
   const sanityInstructor = {
     _key: nanoid(),
     _ref: `collaborator-instructor-${instructor.slug}`,
     _type: "reference",
-  };
+  }
 
-  let courseIllustrator = sanityIllustrator(illustrator);
+  let courseIllustrator = sanityIllustrator(illustrator)
 
   let lessons = resources.map((resource) => {
-    let { title, id, description, image, path } = resource;
+    let { title, id, description, image, path, slug } = resource
     return {
       _key: nanoid(),
       _type: "resource",
+      productionProcessState: "contentReview",
       type: "video",
       externalType: "lesson",
       path,
+      slug: {
+        _type: "slug",
+        current: slug,
+      },
       title,
       externalId: id,
       description,
       image: image.image_url,
       collaborators: [sanityInstructor],
-    };
-  });
+    }
+  })
 
   let collaborators =
     courseIllustrator._ref !== "collaborator-illustrator-null"
       ? [sanityInstructor, courseIllustrator]
-      : [sanityInstructor];
+      : [sanityInstructor]
 
   return {
     _type: "resource",
     type: "course",
     externalType: "playlist",
+    productionProcessState: "contentReview",
+    slug: {
+      _type: "slug",
+      current: slug,
+    },
     externalId: id,
     path,
     title,
@@ -62,19 +72,23 @@ function graphQlToSanity(railsData) {
     image,
     resources: lessons,
     collaborators,
-  };
+  }
 }
 
-let graphQLtoSanityData = graphQlToSanity(railsData);
+let graphQLtoSanityData = graphQlToSanity(railsData)
 //console.log(graphQLtoSanityData);
 
 try {
-  await eggheadSanityClient.create(graphQLtoSanityData);
+  await eggheadSanityClient.create(graphQLtoSanityData)
 } catch (err) {
-  if(err.statusCode === 409) {
+  if (err.statusCode === 409) {
     console.log(err.response.body.error.items[0].error.referenceID)
-    show("Collaborator " + err.response.body.error.items[0].error.referenceID + " does not exist.")
+    show(
+      "Collaborator " +
+        err.response.body.error.items[0].error.referenceID +
+        " does not exist."
+    )
   } else {
-    console.log(err);
+    console.log(err)
   }
 }
